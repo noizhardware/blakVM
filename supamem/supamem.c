c0f9f7807752eeaacc6fadacac1e35896e140893
noizhardware <noisefuel@gmail.com>
2021-04-08 12:34:29 +0200

    nj

--- supamem/supamem.c
+++ supamem/supamem.c
@@ -7,10 +7,20 @@ memaddr_t allocOnPart(memsize_t size, uint8_t partition){
   32x 4bytes blocks
   2bits per block to keep record of their status
   64bits >> 8bytes
+  
+  at the moment I'm using uint16_t absolute addresses, so I can map a total physical memory of MAX 64k
+  
+  I can request to allocate a pool of memory blocks of X size each
+  uint8_t part(memaddr_t startAddress, memsize_t blockSize, memsize_t blocksQty)
+     the function will return SUCCESS if successful and will add the address of the
+     newly created partition in the array "partitions" cacca: dovrebbe almeno buttarmi fuori
+     o un indirizzo o il ID per l'array partitions in modo che posso sapere DOVE l'ha creato
+  structure of allocated partition:
+  [(2bytes)blocksize][(2bytes)how many blocks][status bytes][...actual mem space]
 
 */
 
-#define SUPAMEM_VERSION "2021d07-2136"
+#define SUPAMEM_VERSION "2021d08-1234"
 
 /*** DEFINES */
      #define ERROR 0
@@ -27,6 +37,7 @@ memaddr_t allocOnPart(memsize_t size, uint8_t partition){
      #include <math.h>
 
      #include "bitty.h"
+     #include "baomath.h"
 /* INCLUDES end. */
 
 /*** TYPEDEFS */
@@ -34,6 +45,7 @@ memaddr_t allocOnPart(memsize_t size, uint8_t partition){
      typedef memsize_t memaddr_t;
      typedef uint8_t partitionid_t;
      typedef uint16_t blockid_t;
+     typedef uint8_t blockstatus_t;/* actually using only 2 bits >> [0, 1, 2, 3] */
 /* TYPEDEFS end. */
 
 /*** GLOBALS */
@@ -44,11 +56,17 @@ memaddr_t allocOnPart(memsize_t size, uint8_t partition){
 /* GLOBALS end. */
 
 /*** FUNCTION DECLARATIONS */
-     uint8_t part(memaddr_t startAddress, memsize_t blockSize, memsize_t blocksQty);
-     blockstatus_t twoBitsfromByte(uint8_t byte, uint8_t bits);
-     blockstatus_t getStatus(partitionid_t partId, blockid_t blockNum);
+     uint8_t part(const memaddr_t startAddress, const memsize_t blockSize, const memsize_t blocksQty);
+     
+     blockstatus_t twoBitsfromByte(const uint8_t byte, const uint8_t bitsPos);
+     uint8_t setTwoBitsOnByte(const uint8_t byte, const uint8_t bitsPos, const uint8_t bits);
+     
+     blockstatus_t getStatus(const partitionid_t partId, const blockid_t blockNum);
+     uint8_t setStatus(const partitionid_t partId, const blockid_t blockNum, const uint8_t bits);
+     
      void showmem();
      void showparts();
+     
 /* FUNCTION DECLARATIONS end. */
 
 /*** MAIN */
@@ -58,13 +76,20 @@ int main(){
      showmem();
      showparts();
 
+     printf("part[%u], block[%u] : status -> %u\n", 0, 5, getStatus(0, 5));
+     
+     if(setStatus(0, 5, 2)){
+          printf("part[%u], block[%u] : status -> %u\n", 0, 5, getStatus(0, 5));
+     }
+
+
      return 0;
 }
 /* MAIN end. */
 
 /*** FUNCTION DEFINITIONS */
 
-uint8_t part(memaddr_t startAddress, memsize_t blockSize, memsize_t blocksQty){
+uint8_t part(const memaddr_t startAddress, const memsize_t blockSize, const memsize_t blocksQty){
      memsize_t totSize = blockSize*blocksQty;
      /* how many bytes are needed to keep track of blocks status */
      uint16_t statusBytesQty =
@@ -72,7 +97,7 @@ uint8_t part(memaddr_t startAddress, memsize_t blockSize, memsize_t blocksQty){
      memsize_t newMemFree = memFree - 4 - statusBytesQty; /* (2x)blockSize (2x)blocksQty */
      uint8_t i;
 
-     printf("==== %u blocks >> %u status bytes", blocksQty, statusBytesQty);
+     printf("== %u (%ub)blocks >> %u status bytes (%u of %u %u-bits groups used (%u left in the last byte))\n", blocksQty, blockSize, statusBytesQty, blocksQty, statusBytesQty*8/BLOCK_STATUS_BITS, BLOCK_STATUS_BITS, statusBytesQty*8/BLOCK_STATUS_BITS-blocksQty);
 
      if(totSize>newMemFree || startAddress>=MEM_SIZE){
           return ERROR;
@@ -98,20 +123,87 @@ uint8_t part(memaddr_t startAddress, memsize_t blockSize, memsize_t blocksQty){
      return SUCCESS;
 }/*uint8_t part(memaddr_t startAddress, memsize_t partitionSize, memsize_t partitionsQty)*/
 
-blockstatus_t twoBitsfromByte(uint8_t byte, uint8_t bits){
-     /*bits: 0 = MSB*/
-     if(bits==0){
-          return (byte>>6) & 0xff;
+blockstatus_t twoBitsfromByte(const uint8_t byte, const uint8_t bitsPos){
+     /*bitsPos: 0 = MSB*/
+     if(bitsPos==0){
+          return (byte>>6);
      }
-     else if(bits==1){
-          return (byte>>6) & 0xff;
-     }/*kak*/
+     else if(bitsPos==1){
+          return (byte>>4) & B00000011;
+     }
+     else if(bitsPos==2){
+          return (byte>>2) & B00000011;
+     }
+     else if(bitsPos==3){
+          return byte & B00000011;
+     }
+     else{
+          printf("::== NOPE. bits can only be [0, 1, 2, 3]\n");
+          return -1; /* cacca: return what?? */
+     }
 }
 
-blockstatus_t getStatus(partitionid_t partId, blockid_t blockNum){
-     memaddr_t statusAddr = partitions[partId]+4;
-     /*kak*/
+uint8_t setTwoBitsOnByte(const uint8_t byte, const uint8_t bitsPos, const uint8_t bits){
+     /*bitsPos: 0 = MSB*/
+     if(bitsPos==0){
+          return (byte & B00111111) | (bits<<6);
+     }
+     else if(bitsPos==1){
+          return (byte & B11001111) | (bits<<4);
+     }
+     else if(bitsPos==2){
+          return (byte & B11110011) | (bits<<2);
+     }
+     else if(bitsPos==3){
+          return (byte & B11111100) | bits;
+     }
+     else{
+          printf("::== NOPE. bits can only be [0, 1, 2, 3]\n");
+          return -1; /* cacca: return what?? */
+     }
 }
+
+
+blockstatus_t getStatus(const partitionid_t partId, const blockid_t blockNum){
+     memaddr_t statusAddr = partitions[partId]+4;/* where the status bits-bytes begin */
+     /*
+     ceil(blockNum*BLOCK_STATUS_BITS/8) == which byte contains my 2bits
+     statusAddr + ceil(blockNum*BLOCK_STATUS_BITS/8) == actual address of byte with my 2bits
+     mem[statusAddr + ceil(blockNum*BLOCK_STATUS_BITS/8)] == I'm actually reading the byte from memory
+
+     fract(blockNum/4) will be:
+     .25 >> I select bit 0
+     .5 >> I select bit 1
+     .75 >> I select bit 2
+     0 >> I select bit 3
+     */
+     return twoBitsfromByte(
+          mem[statusAddr + (uint32_t)ceil(blockNum*BLOCK_STATUS_BITS/8)],
+               0 +
+               (fract(blockNum/4)==.5)*1 +
+               (fract(blockNum/4)==.75)*2 +
+               (fract(blockNum/4)==0.)*3
+     );
+}
+
+uint8_t setStatus(const partitionid_t partId, const blockid_t blockNum, const uint8_t bits){
+     memaddr_t statusAddr = partitions[partId]+4;/* where the status bits-bytes begin */
+     
+     mem[statusAddr+(uint32_t)ceil(blockNum*BLOCK_STATUS_BITS/8)] =
+          setTwoBitsOnByte(
+               mem[statusAddr+(uint32_t)ceil(blockNum*BLOCK_STATUS_BITS/8)],
+               0 +
+                    (fract(blockNum/4)==.5)*1 +
+                    (fract(blockNum/4)==.75)*2 +
+                    (fract(blockNum/4)==0.)*3,
+               bits
+          );
+     return SUCCESS;
+     /* cacca: not managing errors */
+     
+     /* kak */
+}
+
 
 void showmem(){
      uint16_t i;
